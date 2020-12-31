82ab8e3b3d75432d026188f830aca53c61ed8ba8
YunHyunJin <21600476@handong.edu>
2020-12-31 21:15:22 +0900

    31 commit

---
+++ lib/tabs/apitab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class apitab extends StatefulWidget {
+  @override
+  _apitabState createState() => _apitabState();
+}
+
+class _apitabState extends State<apitab> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("dadsada"),
+    );
+  }
+}
---
+++ lib/tabs/pdf.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class pdf extends StatefulWidget {
+  @override
+  _pdfState createState() => _pdfState();
+}
+
+class _pdfState extends State<pdf> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
